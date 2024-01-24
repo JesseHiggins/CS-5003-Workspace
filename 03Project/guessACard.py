487a9b325a76987a2e6552fbb9894d301538f685
JesseHiggins <157552708+JesseHiggins@users.noreply.github.com>
2024-01-24 13:21:42 -0500

    make new file cards

---
+++ 03Project/guessACard.py
@@ -0,0 +1,31 @@
+''' This file will be used in Project 3.
+    NAME: Jesse Higgins
+    DATE: January 24, 2024
+    Description of Project 3: Project three introduces command line arguments and then builds functions
+'''
+# Put any libararies to be imported below
+import random
+
+def getSuit():
+    ''' Describe getSuit here '''
+    #replace the following line with your implementation
+    pass
+
+def getValue():
+    ''' Describe getValue here '''
+    #replace the following line with your implementation
+    pass
+
+def printCard(value, suit):
+    ''' Describe printCard here '''
+    #replace the following line with your implementation
+    pass
+
+def main():
+    ''' Describe what the program will do here '''
+    #replace the pass below with your implementation
+    #suggestion: use this function to test getSuit, getValue and printCard 
+    #before implementing the main described in the Project 3 description
+    pass
+
+#remember to put in the lines to call main() that are described in part 6
